93422724ded0e0ed2c5f7cb3f09318705815a198
Adheep KeyValue <adheep@keyvalue.systems>
2025-07-28 16:51:34 +0530

    CRM-3170: Update dealer dropdown data

--- dealers.js
+++ dealers.js
@@ -1,14 +1,44 @@
 // Paste yout JSON string containing dealer data here
-const dealersJSON = `[
+const dealersJSON = `
+[
+  {
+    "dealerId": "12896180",
+    "dealerName": "Tender Cars"
+  },
+  {
+    "dealerId": "13700",
+    "dealerName": "Mulder-Mazda 't Groene Hart"
+  },
+  {
+    "dealerId": "14250",
+    "dealerName": "Mulder-Mazda Rotterdam-Noord"
+  },
   {
     "dealerId": "14401",
     "dealerName": "Mazda Nederland"
   },
   {
+    "dealerId": "548388680",
+    "dealerName": "Mulder-Mazda Rotterdam-West"
+  },
+  {
+    "dealerId": "553106381",
+    "dealerName": "Target Consignment"
+  },
+  {
+    "dealerId": "553650130",
+    "dealerName": "Occasion Kampioen"
+  },
+  {
+    "dealerId": "555603767",
+    "dealerName": "test-olcay"
+  },
+  {
     "dealerId": "698351",
     "dealerName": "Mulder Mazda Rotterdam-Zuid"
   }
-]`;
+]
+  `;
 
 // Convert JSON to JavaScript object
 export const dealers = JSON.parse(dealersJSON);
